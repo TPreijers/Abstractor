48f893a2e4770d21158856d8fe0dc36d9cde3867
TPreijers <TPreijers@users.noreply.github.com>
2023-07-29 18:36:14 +0200

    aa

---
+++ R/Setup.R
@@ -0,0 +1,15 @@
+library(devtools)
+library(roxygen2)
+
+create_package("C:/Users/TPreijers/Documents/Abstractor", open = F, roxygen = T, check_name = F, rstudio = T)
+
+use_git()
+
+# use_r("Extractor")
+# use_r("Receiver")
+# use_r("Builder")
+
+check()
+document()
+
+
